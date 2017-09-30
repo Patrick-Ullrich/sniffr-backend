51d320af1c55842049539ab552d04f887ad00ace
Patrick Ullrich <patrick.ullrich93@gmail.com>
2017-09-30 09:28:15 -0600

    asdf

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,3 @@
 var AUTH0_CLIENT_ID = 'ixMOnysTLo3gcyyzOdBiZpJ1J0u2jSr9';
 var AUTH0_DOMAIN = 'patrickullrich.auth0.com';
-var AUTH0_CALLBACK_URL = 'http://localhost:3000/callback';
+var AUTH0_CALLBACK_URL = process.env.NODE_ENV == 'production' ? 'https://rezqs.herokuapp.com/callback' : 'http://localhost:3000/callback';
