f4b46fc026debff4397864ee004ffca9a5ab0ecc
Patrick Ullrich <patrick.ullrich93@gmail.com>
2017-11-14 23:32:57 -0600

    foster

--- models/phone.js
+++ models/phone.js
@@ -1,22 +1,26 @@
-'use strict';
-module.exports = function (sequelize, DataTypes) {
-    var Phone = sequelize.define('Phone', {
-        phoneId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true,
-            field: 'phone_id'
-        },
-        phoneNumber: {
-            type: DataTypes.TEXT,
-            field: 'phone_number'
-        }
-    }, {
-        underscored: true,
-        underscoredAll: true,
-        version: true
-    });
+"use strict";
+module.exports = function(sequelize, DataTypes) {
+  var Phone = sequelize.define(
+    "Phone",
+    {
+      phoneId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        field: "phone_id"
+      },
+      phoneNumber: {
+        type: DataTypes.TEXT,
+        field: "phone_number"
+      }
+    },
+    {
+      underscored: true,
+      underscoredAll: true,
+      version: true
+    }
+  );
 
-    return Phone;
+  return Phone;
 };
