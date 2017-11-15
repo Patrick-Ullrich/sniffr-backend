df8a31fd6d6c117171f167f4ffef72d299948c93
Patrick Ullrich <patrick.ullrich93@gmail.com>
2017-11-14 22:59:37 -0600

    fixed incident update

--- routes/api/incident/index.js
+++ routes/api/incident/index.js
@@ -1,58 +1,59 @@
-const router = require('express').Router();
-const IncidentService = require('../../../services/incidentService');
-const boom = require('express-boom');
-const Sequelize = require('../../../models/');
+const router = require("express").Router();
+const IncidentService = require("../../../services/incidentService");
+const boom = require("express-boom");
+const Sequelize = require("../../../models/");
 
-router.get('/', (req, res) => {
-    IncidentService.findAll()
-        .then((result) => {
-            res.status(200).json(result);
-        });
+router.get("/", (req, res) => {
+  IncidentService.findAll().then(result => {
+    res.status(200).json(result);
+  });
 });
 
-router.get('/:incident_id', (req, res) => {
-    IncidentService.findById(req.params.incident_id)
-        .then((result) => {
-            if (!result) {
-                res.boom.notFound();
-            } else {
-                res.status(200).json(result);
-            }
-        });
+router.get("/:incident_id", (req, res) => {
+  IncidentService.findById(req.params.incident_id).then(result => {
+    if (!result) {
+      res.boom.notFound();
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
-router.post('/', (req, res) => {
-    IncidentService.create(req.body)
-        .then(() => {
-            res.status(201).json({});
-        }).catch((err) => {
-            if (err.name === "SequelizeValidationError") {
-                res.boom.badRequest("Validation Error", err);
-            }
-        });
+router.post("/", (req, res) => {
+  IncidentService.create(req.body)
+    .then(() => {
+      res.status(201).json({});
+    })
+    .catch(err => {
+      if (err.name === "SequelizeValidationError") {
+        res.boom.badRequest("Validation Error", err);
+      }
+    });
 });
 
-router.put('/:incident_id', (req, res) => {
-    IncidentService.update(req.params.incident_id, req.body)
-        .then(() => {
-            res.status(200).json({});
-        }).catch((err) => {
-            if (err.name === "SequelizeValidationError") {
-                res.boom.badRequest("Validation Error", err);
-            }
-        });
+router.put("/:incident_id", (req, res) => {
+  IncidentService.update(req.params.incident_id, req.body)
+    .then(() => {
+      res.status(200).json({});
+    })
+    .catch(err => {
+      if (err.name === "SequelizeValidationError") {
+        res.boom.badRequest("Validation Error", err);
+      }
+    });
 });
 
-router.delete('/:incident_id', (req, res) => {
-    IncidentService.delete(req.params.incident_id)
-        .then((count) => {
-            if (!count) {
-                res.boom.notFound();
-            }
-            res.status(200).json({});
-        }).catch((err) => {
-            res.boom.badRequest();
-        });
+router.delete("/:incident_id", (req, res) => {
+  IncidentService.delete(req.params.incident_id)
+    .then(count => {
+      if (!count) {
+        res.boom.notFound();
+      }
+      res.status(200).json({});
+    })
+    .catch(err => {
+      res.boom.badRequest();
+    });
 });
 
 module.exports = router;
--- services/incidentService.js
+++ services/incidentService.js
@@ -1,75 +1,88 @@
-const models = require('../models/');
+const models = require("../models/");
 
 module.exports = IncidentService = {
-    findAll: () => {
-        return new Promise((resolve, reject) => {
-            models.Incident.findAll()
-                .then((result) => {
-                    resolve(result);
-                });
+  findAll: () => {
+    return new Promise((resolve, reject) => {
+      models.Incident.findAll().then(result => {
+        resolve(result);
+      });
+    });
+  },
+  findById: incidentId => {
+    return new Promise((resolve, reject) => {
+      models.Incident
+        .find({
+          where: {
+            incident_id: incidentId
+          }
+        })
+        .then(result => {
+          resolve(result);
         });
-    },
-    findById: (incidentId) => {
-        return new Promise((resolve, reject) => {
-            models.Incident.find({
-                where: {
-                    incident_id: incidentId
-                }
-            }).then((result) => {
-                resolve(result);
-            });
+    });
+  },
+  create: incident => {
+    return new Promise((resolve, reject) => {
+      models.Incident
+        .create({
+          severityTypeId: incident.severityTypeId,
+          incidentTypeId: incident.incidentTypeId,
+          incidentStatusId: incident.incidentStatusId,
+          dogId: incident.dogId,
+          requestingPartyId: incident.requestingPartyId,
+          message: incident.message
+        })
+        .then(() => {
+          console.log("resolved");
+          resolve();
+        })
+        .catch(err => {
+          console.log("err");
+          console.log(err);
+          reject(err);
         });
-    },
-    create: (incident) => {
-        return new Promise((resolve, reject) => {
-            models.Incident.create({
-                severityTypeId: incident.severityTypeId,
-                incidentTypeId: incident.incidentTypeId,
-                incidentStatusId: incident.incidentStatusId,
-                dogId: incident.dogId,
-                requestingPartyId: incident.requestingPartyId,
-                message: incident.message
-            }).then(() => {
-                console.log('resolved');
-                resolve();
-            }).catch((err) => {
-                console.log('err');
-                console.log(err);
-                reject(err);
-            });
+    });
+  },
+  update: (incidentId, incident) => {
+    return new Promise((resolve, reject) => {
+      models.Incident
+        .update(
+          {
+            severityTypeId: incident.severityTypeId,
+            incidentTypeId: incident.incidentTypeId,
+            incidentStatusId: incident.incidentStatusId,
+            dogId: incident.dogId,
+            requestingPartyId: incident.requestingPartyId,
+            message: incident.message
+          },
+          {
+            where: {
+              incident_id: incidentId
+            }
+          }
+        )
+        .then(updateCount => {
+          resolve();
+        })
+        .catch(err => {
+          reject(err);
         });
-    },
-    update: (incidentId, incident) => {
-        return new Promise((resolve, reject) => {
-            models.Incident.update({
-                severityTypeId: incident.severityTypeId,
-                incidentTypeId: incident.incidentTypeId,
-                incidentStatusId: incident.incidentStatusId,
-                dogId: incident.dogId,
-                requestingPartyId: incident.requestingPartyId,
-                message: message
-            }, {
-                where: {
-                    incident_id: incidentId
-                }
-            }).then(updateCount => {
-                resolve();
-            }).catch(err => {
-                reject(err);
-            });
+    });
+  },
+  delete: incidentId => {
+    return new Promise((resolve, reject) => {
+      models.Incident
+        .destroy({
+          where: {
+            incident_id: incidentId
+          }
+        })
+        .then(count => {
+          resolve(count);
+        })
+        .catch(err => {
+          reject(err);
         });
-    },
-    delete: (incidentId) => {
-        return new Promise((resolve, reject) => {
-            models.Incident.destroy({
-                where: {
-                    incident_id: incidentId
-                }
-            }).then(count => {
-                resolve(count);
-            }).catch(err => {
-                reject(err);
-            })
-        });
-    }
+    });
+  }
 };
